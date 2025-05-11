8ac5f103f064229daeb7fc69485e6ab371928eb5
Zulayhobonu <zulayhotillavoldiyeva@gmail.com>
2025-05-11 14:55:58 +0300

    childda ikkita conteyner

--- yelloow/lib/main.dart
+++ yelloow/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: asos(),));
+void main() {
+  runApp(MaterialApp(home: asos()));
 }
 
 class asos extends StatefulWidget {
@@ -14,6 +14,45 @@ class asos extends StatefulWidget {
 class _asosState extends State<asos> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(width: double.infinity,height: 300,color: Colors.red,),);
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 290, left: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 280, bottom: 250),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 300),
+            child: Text("data"),
+          )],
+        ),
+        width: double.infinity,
+        height: 400,
+        color: Colors.red,
+      ),
+    );
   }
 }
