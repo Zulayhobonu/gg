552af82a92dbeceec2522e8bfda51d5fcf88779d
Zulayhobonu <zulayhotillavoldiyeva@gmail.com>
2025-05-11 15:29:32 +0300

    sentorda 3 ta text va 1 ta conteyner

---
+++ yelloow/lib/bir.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class bir extends StatefulWidget {
+  const bir({super.key});
+
+  @override
+  State<bir> createState() => _birState();
+}
+
+class _birState extends State<bir> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- yelloow/lib/main.dart
+++ yelloow/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yelloow/bir.dart';
 
 void main() {
   runApp(MaterialApp(home: asos()));
@@ -16,38 +17,96 @@ class _asosState extends State<asos> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:CircleAvatar(radius: 30,backgroundImage: AssetImage("rasm/lavash.jpg"))
-      ),
-      body: Container(
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 290, left: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                ),
-                width: 100,
-                height: 100,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 280, bottom: 250),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                ),
-                width: 100,
-                height: 100,
-              ),
-            ),
-          ],
+        title: CircleAvatar(
+          radius: 30,
+          backgroundImage: AssetImage("rasm/lavash.jpg"),
         ),
-        width: double.infinity,
-        height: 400,
-        color: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 290, left: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 280, bottom: 250),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
+            width: double.infinity,
+            height: 400,
+            color: Colors.red,
+          ),
+          SizedBox(height: 15),
+          Center(
+            child: Text(
+              "Freshness at",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+            ),
+          ),
+          Center(
+            child: Text(
+              "your fingertips",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Tap to the best picks, deliwered fast to your\n       door no fuss all fresh alweys ready",
+            ),
+          ),
+          SizedBox(height: 30),
+          InkWell(onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context)=>bir()));},
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+              width: 300,
+              height: 80,
+              child: Row(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        width: 50,
+                        height: 50,
+                        child: Icon(Icons.lock, size: 30),
+                      ),
+                    ),
+                  ),SizedBox(width: 30,),
+                  Text(
+                    "Get start",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),SizedBox(width: 50,),
+               Icon(Icons.arrow_forward,color: Colors.white,) ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
