605a7d86815f226ff6da77d1690425c1546cee3d
Zulayhobonu <zulayhotillavoldiyeva@gmail.com>
2025-05-11 15:11:41 +0300

    abbarda bitta rasm

--- yelloow/lib/main.dart
+++ yelloow/lib/main.dart
@@ -15,39 +15,35 @@ class _asosState extends State<asos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title:CircleAvatar(radius: 30,backgroundImage: AssetImage("rasm/lavash.jpg"))
+      ),
       body: Container(
-        child: Column(
+        child: Row(
           children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 290, left: 10),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    width: 100,
-                    height: 100,
-                  ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 290, left: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 280, bottom: 250),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
-              ],
+                width: 100,
+                height: 100,
+              ),
             ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 300),
-            child: Text("data"),
-          )],
+            Padding(
+              padding: const EdgeInsets.only(left: 280, bottom: 250),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+                width: 100,
+                height: 100,
+              ),
+            ),
+          ],
         ),
         width: double.infinity,
         height: 400,
